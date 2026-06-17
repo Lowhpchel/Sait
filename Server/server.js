48fbecb98bd80430acb4d4fab7462a3ace96b29c
Lowhpchel <vova.ilin12@yandex.ru>
2026-06-17 22:24:11 +0400

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ import errorHandler from './middleware/errorHandler.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
